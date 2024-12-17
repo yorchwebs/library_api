a68d162545306a6e03502d454f35feb6de3cbfd0
Jorge Garcia <yorchwebs@gmail.com>
2024-12-17 15:39:07 -0600

    First Commit

---
+++ books/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import ListView, DetailView
+from .models import Book
+
+
+# Create your views here.
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
